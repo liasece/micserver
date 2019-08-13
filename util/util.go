e266db10472f1107da156da540c24006470ee5ec
Jansen <liasece@gmail.com>
2019-08-13 11:17:14 +0800

    add CSPRNG support

--- util/util.go
+++ util/util.go
@@ -18,6 +18,7 @@ import (
 	"unsafe"
 
 	"crypto/hmac"
+	crand "crypto/rand"
 	"crypto/sha1"
 	"crypto/sha256"
 )
@@ -96,6 +97,31 @@ func HmacSha256ByString(contentstr string, keystr string) string {
 	content := StringToBytes(contentstr)
 	key := StringToBytes(keystr)
 	return HmacSha256(content, key)
+}
+
+// GenerateRandomBytes returns securely generated random bytes.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := crand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// GenerateRandomString returns a URL-safe, base64 encoded
+// securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return hex.EncodeToString(b), err
 }
 
 func HmacSha1(content []byte, key []byte) string {
